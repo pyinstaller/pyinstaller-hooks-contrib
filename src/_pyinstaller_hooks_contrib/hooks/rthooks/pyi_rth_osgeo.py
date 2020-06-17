318f05136e971a0f5757d93cf1d6d34edc913e26
Legorooj <legorooj@protonmail.com>
2020-06-17 12:04:40 +1000

    Move some runtime hooks from the PyInstaller main repository

--- src/_pyinstaller_hooks_contrib/hooks/rthooks/pyi_rth_osgeo.py
+++ src/_pyinstaller_hooks_contrib/hooks/rthooks/pyi_rth_osgeo.py
@@ -1,13 +1,13 @@
-# ------------------------------------------------------------------
-# Copyright (c) 2020 PyInstaller Development Team.
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015-2020, PyInstaller Development Team.
 #
-# This file is distributed under the terms of the Apache License 2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 #
-# The full license is available in LICENSE.APL.txt, distributed with
-# this software.
+# The full license is in the file COPYING.txt, distributed with this software.
 #
 # SPDX-License-Identifier: Apache-2.0
-# ------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
 import os
 import sys
