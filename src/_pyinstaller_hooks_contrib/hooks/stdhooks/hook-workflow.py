432b989f35f52f16ed352c211f5fe34c55f22873
zhouronghua <zhou.ronghua@zte.com.cn>
2020-07-30 13:45:29 +0800

    Update hook-workflow.py
    delete #-----

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-workflow.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-workflow.py
@@ -1,4 +1,3 @@
-#-----------------------------------------------------------------------------
 # Copyright (c) 2013-2020, PyInstaller Development Team.
 #
 # Distributed under the terms of the GNU General Public License (version 2
@@ -7,6 +6,6 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
-#-----------------------------------------------------------------------------
+
 from PyInstaller.utils.hooks import copy_metadata
 datas = copy_metadata('workflow')
