8c49be541e3f2fdf6422649f79ed3826e3d07433
shoeshine <45012455+amifunny@users.noreply.github.com>
2020-08-03 20:40:42 +0530

    Update hook-gmplot.py

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-gmplot.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-gmplot.py
@@ -1,15 +1,14 @@
 # ------------------------------------------------------------------
-# Copyright (c) 2020 PyInstaller Development Team.
+# Copyright (c) 2005-2020 PyInstaller Development Team.
 #
-# This file is distributed under the terms of the GNU General Public
-# License (version 2.0 or later).
+# This file is distributed under the terms of the Apache License 2.0
 #
-# The full license is available in LICENSE.GPL.txt, distributed with
+# The full license is available in LICENSE.APL.txt, distributed with
 # this software.
 #
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: Apache-2.0
 # ------------------------------------------------------------------
 
 from PyInstaller.utils.hooks import collect_data_files
 
-datas = collect_data_files('gmplot') 
+datas = collect_data_files('gmplot')
