29ba60def0db5219e3d23441b74ce831af24a5e6
shoeshine <45012455+amifunny@users.noreply.github.com>
2020-08-04 12:13:42 +0530

    Change LICENSE info to GPL

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-gmplot.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-gmplot.py
@@ -1,12 +1,13 @@
 # ------------------------------------------------------------------
 # Copyright (c) 2005-2020 PyInstaller Development Team.
 #
-# This file is distributed under the terms of the Apache License 2.0
+# This file is distributed under the terms of the GNU General Public
+# License (version 2.0 or later).
 #
-# The full license is available in LICENSE.APL.txt, distributed with
+# The full license is available in LICENSE.GPL.txt, distributed with
 # this software.
 #
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
 from PyInstaller.utils.hooks import collect_data_files
