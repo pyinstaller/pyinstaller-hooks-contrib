689c209940d9cc39ea81c974fc3c0a1e112e213e
amifunny <hemantsingh.buenodev@gmail.com>
2020-08-17 10:14:39 +0530

    Add pyproj runtime hook for conda env

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pyproj.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pyproj.py
@@ -1,15 +1,29 @@
-# ------------------------------------------------------------------
-# Copyright (c) 2020 PyInstaller Development Team.
-#
-# This file is distributed under the terms of the GNU General Public
-# License (version 2.0 or later).
-#
-# The full license is available in LICENSE.GPL.txt, distributed with
-# this software.
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-# ------------------------------------------------------------------
-
+import os
+import sys
 from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.compat import is_win
+
+
+hiddenimports = [
+    "pyproj.datadir"
+]
 
 datas = collect_data_files('pyproj')
+
+if hasattr(sys, 'real_prefix'):  # check if in a virtual environment
+    root_path = sys.real_prefix
+else:
+    root_path = sys.prefix
+
+# - conda-specific
+if is_win:
+    tgt_proj_data = os.path.join('Library', 'share', 'proj')
+    src_proj_data = os.path.join(root_path, 'Library', 'share', 'proj')
+
+else:  # both linux and darwin
+    tgt_proj_data = os.path.join('share', 'proj')
+    src_proj_data = os.path.join(root_path, 'share', 'proj')
+
+if os.path.exists(src_proj_data):
+    datas.append((src_proj_data, tgt_proj_data))
+    # a real-time hook takes case to define the path for `PROJ_LIB`
