8838f509ba8eba3c687b34aae333e8558a932a8b
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-08-26 23:06:19 +0200

    hooks: tensorflow: rework the hook to improve compatibility across different versions
    Clean and up and rework the tensorflow hook to improve compatibility
    across different tensorflow versions.
    
    Currently, the `tensorflow` hook collects the submodules from
    `tensorflow_core` in a rather roundabout way: by specifying
    `tensorflow_core` as a hidden import, and relying on this to pull in
    `tensorflow_core.python`. The hook for `tensorflow_core.python` is
    the one that actually collects data and submodules from `tensorflow_core`.
    
    This mechanism is brittle, and seems to break on:
    * tensorflow 1.15.x: due to import rerouting between `tensorflow_core`
    and `tensorflow`, the `tensorflow_core.python` is not pulled in
    * tensorflow 2.2.0+: the `tensorflow_core` module has been removed, and
    its contents now reside within `tensorflow` itself.
    
    To fix this issue and improve compatibility across different versions
    of tensorflow, the `tensorflow_core.python` hook is removed, and
    `tensorflow` hook is modified to collect data and submodules from
    either `tensorflow` or `tensorflow_core`, depending on version:
    * 1.14.x and earlier: collect from `tensorflow`
    * 1.15.x - 2.1.x: collect from `tensorflow_core`
    * 2.2x and later: collect from `tensorflow`

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tensorflow.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tensorflow.py
@@ -10,4 +10,23 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-hiddenimports = ['tensorflow_core']
+from PyInstaller.utils.hooks import is_module_satisfies, \
+    collect_submodules, collect_data_files
+
+if is_module_satisfies("tensorflow < 1.15.0"):
+    # 1.14.x and earlier: collect everything from tensorflow
+    hiddenimports = collect_submodules('tensorflow')
+    datas = collect_data_files('tensorflow')
+elif is_module_satisfies("tensorflow >= 1.15.0") and is_module_satisfies("tensorflow < 2.2.0"):
+    # 1.15.x - 2.1.x: collect everything from tensorflow_core
+    hiddenimports = collect_submodules('tensorflow_core')
+    datas = collect_data_files('tensorflow_core')
+
+    # Under 1.15.x, we seem to fail collecting a specific submodule,
+    # and need to add it manually...
+    if is_module_satisfies("tensorflow >= 1.15.0") and is_module_satisfies("tensorflow < 2.0.0"):
+        hiddenimports += ['tensorflow_core._api.v1.compat.v2.summary.experimental']
+else:
+    # 2.2.0 and newer: collect everything from tensorflow again
+    hiddenimports = collect_submodules('tensorflow')
+    datas = collect_data_files('tensorflow')
