95f87e90c0633821fffef286f240fadfd684bd85
Mohamed Feddad <mrf345@gmail.com>
2020-09-13 21:49:25 +0300

    Remove unneeded include_py_files arguement.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-flask_restx.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-flask_restx.py
@@ -10,4 +10,4 @@
 # -----------------------------------------------------------------------------
 from PyInstaller.utils.hooks import collect_data_files
 
-datas = collect_data_files('flask_restx', include_py_files=True)
+datas = collect_data_files('flask_restx')
