1dd688811e3749a24ac29af92b2ed15d3ad9d7bb
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-09-14 12:22:23 +0200

    hooks: tensorflow: minimize the use of `is_module_satisfies()`
    A minor cleanup; use `is_module_satisfies()` to define version
    related booleans, and then use those in the if/else blocks to make
    them easier to read.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tensorflow.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tensorflow.py
@@ -13,19 +13,25 @@
 from PyInstaller.utils.hooks import is_module_satisfies, \
     collect_submodules, collect_data_files
 
-if is_module_satisfies("tensorflow < 1.15.0"):
+tf_pre_1_15_0 = is_module_satisfies("tensorflow < 1.15.0")
+tf_post_1_15_0 = is_module_satisfies("tensorflow >= 1.15.0")
+tf_pre_2_0_0 = is_module_satisfies("tensorflow < 2.0.0")
+tf_pre_2_2_0 = is_module_satisfies("tensorflow < 2.2.0")
+
+if tf_pre_1_15_0:
     # 1.14.x and earlier: collect everything from tensorflow
     hiddenimports = collect_submodules('tensorflow')
     datas = collect_data_files('tensorflow')
-elif is_module_satisfies("tensorflow >= 1.15.0") and is_module_satisfies("tensorflow < 2.2.0"):
+elif tf_post_1_15_0 and tf_pre_2_2_0:
     # 1.15.x - 2.1.x: collect everything from tensorflow_core
     hiddenimports = collect_submodules('tensorflow_core')
     datas = collect_data_files('tensorflow_core')
 
     # Under 1.15.x, we seem to fail collecting a specific submodule,
     # and need to add it manually...
-    if is_module_satisfies("tensorflow >= 1.15.0") and is_module_satisfies("tensorflow < 2.0.0"):
-        hiddenimports += ['tensorflow_core._api.v1.compat.v2.summary.experimental']
+    if tf_post_1_15_0 and tf_pre_2_0_0:
+        hiddenimports += \
+            ['tensorflow_core._api.v1.compat.v2.summary.experimental']
 else:
     # 2.2.0 and newer: collect everything from tensorflow again
     hiddenimports = collect_submodules('tensorflow')
