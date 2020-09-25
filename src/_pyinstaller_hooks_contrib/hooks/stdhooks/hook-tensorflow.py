fb16f21d09bcdb43a54b170bb175332e2948c536
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-09-25 04:27:28 +0200

    Fix library duplication in tensorflow v.2.3 and avoid packaging unnecessary files (#50)
    * hooks: tensorflow: prevent duplication of _pywrap_tensorflow_internal
    
    On some tensorflow versions (2.3.0 thus far), the
    _pywrap_tensorflow_internal extension module ends up duplicated.
    We pick it up as both an extension module (placed in
    `tensorflow/python` directory) and as a shared library (placed in
    program's root directory). This increases the program size, and
    also prevents the program from running under macOS.
    
    In the problematic versions, the extension module seems to be
    picked up as `tensorflow.python._pywrap_tensorflow_internal`,
    while in others it is picked up as just ˙_pywrap_tensorflow_internal`.
    So we try to work around the duplication by removing the problematic
    entry from `hiddenimports` and adding it to `excludedimports`.
    
    Fixes #49.
    
    * hooks: tensorflow: avoid collecting unnecessary data
    
    Avoid collecting tensorflow's development headers in `include`
    directory and XLA AOT runtime sources (`xla_aot_runtime_src`).
    
    Also prevent `libtensorflow_framework` shared library from being
    picked up as data to prevent its duplication (it should be
    correctly picked up as a shared library by dependency scanner).

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tensorflow.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tensorflow.py
@@ -18,14 +18,41 @@ tf_post_1_15_0 = is_module_satisfies("tensorflow >= 1.15.0")
 tf_pre_2_0_0 = is_module_satisfies("tensorflow < 2.0.0")
 tf_pre_2_2_0 = is_module_satisfies("tensorflow < 2.2.0")
 
+
+# Exclude from data collection:
+#  - development headers in include subdirectory
+#  - XLA AOT runtime sources
+#  - libtensorflow_framework shared library (to avoid duplication)
+data_excludes = [
+    "include",
+    "xla_aot_runtime_src",
+    "libtensorflow_framework.*"
+]
+
+# Under tensorflow 2.3.0 (the most recent version at the time of writing),
+# _pywrap_tensorflow_internal extension module ends up duplicated; once
+# as an extension, and once as a shared library. In addition to increasing
+# program size, this also causes problems on macOS, so we try to prevent
+# the extension module "variant" from being picked up.
+#
+# See pyinstaller/pyinstaller-hooks-contrib#49 for details.
+excluded_submodules = ['tensorflow.python._pywrap_tensorflow_internal']
+
+
+def _submodules_filter(x):
+    return x not in excluded_submodules
+
+
 if tf_pre_1_15_0:
     # 1.14.x and earlier: collect everything from tensorflow
-    hiddenimports = collect_submodules('tensorflow')
-    datas = collect_data_files('tensorflow')
+    hiddenimports = collect_submodules('tensorflow',
+                                       filter=_submodules_filter)
+    datas = collect_data_files('tensorflow', excludes=data_excludes)
 elif tf_post_1_15_0 and tf_pre_2_2_0:
     # 1.15.x - 2.1.x: collect everything from tensorflow_core
-    hiddenimports = collect_submodules('tensorflow_core')
-    datas = collect_data_files('tensorflow_core')
+    hiddenimports = collect_submodules('tensorflow_core',
+                                       filter=_submodules_filter)
+    datas = collect_data_files('tensorflow_core', excludes=data_excludes)
 
     # Under 1.15.x, we seem to fail collecting a specific submodule,
     # and need to add it manually...
@@ -34,5 +61,8 @@ elif tf_post_1_15_0 and tf_pre_2_2_0:
             ['tensorflow_core._api.v1.compat.v2.summary.experimental']
 else:
     # 2.2.0 and newer: collect everything from tensorflow again
-    hiddenimports = collect_submodules('tensorflow')
-    datas = collect_data_files('tensorflow')
+    hiddenimports = collect_submodules('tensorflow',
+                                       filter=_submodules_filter)
+    datas = collect_data_files('tensorflow', excludes=data_excludes)
+
+excludedimports = excluded_submodules
