5211e7b4df77c3ecb503a172d2f5f4bc8d3b12d6
Dan Yeaw <dan@yeaw.me>
2020-10-03 21:02:35 -0400

    Exclude tinycss2 hook for versions after 1.0.0
    tinycss2 #27 updated the packaging which removes the need for a hook.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tinycss2.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-tinycss2.py
@@ -12,11 +12,12 @@
 
 
 """
-Hook for tinycsss2. tinycss2 is a low-level CSS parser and generator.
+Hook for tinycss2. tinycss2 is a low-level CSS parser and generator.
 https://github.com/Kozea/tinycss2
 """
 from PyInstaller.utils.hooks import collect_data_files
 
 
+# Hook no longer required for tinycss2 >= 1.0.0
 def hook(hook_api):
     hook_api.add_datas(collect_data_files(hook_api.__name__))
