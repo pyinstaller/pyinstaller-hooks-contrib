4066adbbe28d456f7c4c0afbdc94638e1696120b
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-10-21 18:45:16 +0200

    hooks: zmq: avoid collecting cffi backend altogether
    cffi backend requires compilation of C extension module at runtime,
    which appears to be broken in frozen program. Even after collecting
    the necessary data files, importing fails with:
    ```
    Failed to import zm.backend.cffi: PyZMQ CFFI backend couldn't find
    zeromq: invalid command 'build'
    ```
    which originates in cffi/distutils.
    
    This means that zmq.backend.cffi never really worked with frozen
    program. So avoid collecting it altogether, and always use cython
    backend (which seems to be first choice in most cases, anyway).

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-zmq.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-zmq.py
@@ -15,10 +15,9 @@
 Hook for PyZMQ. Cython based Python bindings for messaging library ZeroMQ.
 http://www.zeromq.org/
 """
-from PyInstaller.utils.hooks import collect_submodules, collect_data_files
+from PyInstaller.utils.hooks import collect_submodules
 
 hiddenimports = ['zmq.utils.garbage']
-datas = []
 
 # PyZMQ comes with two backends, cython and cffi. Calling collect_submodules()
 # on zmq.backend seems to trigger attempt at compilation of C extension
@@ -33,5 +32,12 @@ hiddenimports += collect_submodules('zmq.backend.cython')
 
 # cffi backend: contains extra data that needs to be collected
 # (e.g., _cdefs.h)
-hiddenimports += collect_submodules('zmq.backend.cffi')
-datas += collect_data_files('zmq.backend.cffi', excludes=['**/__pycache__', ])
+#
+# NOTE: the cffi backend requires compilation of C extension at runtime,
+# which appears to be broken in frozen program. So avoid collecting
+# it altogether...
+if False:
+    from PyInstaller.utils.hooks import collect_data_files
+
+    hiddenimports += collect_submodules('zmq.backend.cffi')
+    datas = collect_data_files('zmq.backend.cffi', excludes=['**/__pycache__', ])
