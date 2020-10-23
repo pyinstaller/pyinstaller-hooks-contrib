6b9b9412457b9a31c244e3c0601e7c79518e16a7
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-10-23 06:45:02 +0200

    hooks: zmq: avoid triggering compilation of C extension for cffi backend (#59)
    * hooks: zmq: separately collect the backends
    
    Collecting all submodules of zmq.backend appears to trigger attempt
    at compiling the C extension for the cffi backend, whose failure in
    non-English locales may trigger UnicodeDecodeError, as reported in
    pyinstaller/pyinstaller#2258.
    
    Collecting each submodule individually does not seem to exhibit
    this problem.
    
    * hooks: zmq: collect required data from cffi backend
    
    cffi backend requires the C source and header files that are
    located in its folder.
    
    * hooks: zmq: avoid collecting cffi backend altogether
    
    cffi backend requires compilation of C extension module at runtime,
    which appears to be broken in frozen program. Even after collecting
    the necessary data files, importing fails with:
    ```
    Failed to import zm.backend.cffi: PyZMQ CFFI backend couldn't find
    zeromq: invalid command 'build'
    ```
    which originates in cffi/distutils.
    
    This means that zmq.backend.cffi never really worked with frozen
    program. So avoid collecting it altogether, and always use cython
    backend (which seems to be first choice in most cases, anyway).

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-zmq.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-zmq.py
@@ -15,7 +15,29 @@
 Hook for PyZMQ. Cython based Python bindings for messaging library ZeroMQ.
 http://www.zeromq.org/
 """
-from PyInstaller.utils.hooks import collect_submodules, get_module_file_attribute
-from PyInstaller.compat import is_win
+from PyInstaller.utils.hooks import collect_submodules
 
-hiddenimports = ['zmq.utils.garbage'] + collect_submodules('zmq.backend')
+hiddenimports = ['zmq.utils.garbage']
+
+# PyZMQ comes with two backends, cython and cffi. Calling collect_submodules()
+# on zmq.backend seems to trigger attempt at compilation of C extension
+# module for cffi backend, which will fail if ZeroMQ development files
+# are not installed on the system. On non-English locales, the resulting
+# localized error messages may cause UnicodeDecodeError. Collecting each
+# backend individually, however, does not seem to cause any problems.
+hiddenimports += ['zmq.backend']
+
+# cython backend
+hiddenimports += collect_submodules('zmq.backend.cython')
+
+# cffi backend: contains extra data that needs to be collected
+# (e.g., _cdefs.h)
+#
+# NOTE: the cffi backend requires compilation of C extension at runtime,
+# which appears to be broken in frozen program. So avoid collecting
+# it altogether...
+if False:
+    from PyInstaller.utils.hooks import collect_data_files
+
+    hiddenimports += collect_submodules('zmq.backend.cffi')
+    datas = collect_data_files('zmq.backend.cffi', excludes=['**/__pycache__', ])
