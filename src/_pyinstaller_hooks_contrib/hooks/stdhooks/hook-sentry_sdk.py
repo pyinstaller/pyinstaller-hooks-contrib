dd496d47e9373b09da811bcd9902c1cc03f95722
SimonIT <simonit.orig@gmail.com>
2020-11-19 14:55:49 +0100

    Use exec_statement and add comment for _AUTO_ENABLING_INTEGRATIONS

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-sentry_sdk.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-sentry_sdk.py
@@ -9,7 +9,8 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
-import sentry_sdk.integrations as si
+import json
+from PyInstaller.utils.hooks import exec_statement
 
 hiddenimports = ["sentry_sdk.integrations.stdlib",
                  "sentry_sdk.integrations.excepthook",
@@ -20,8 +21,20 @@ hiddenimports = ["sentry_sdk.integrations.stdlib",
                  "sentry_sdk.integrations.logging",
                  "sentry_sdk.integrations.threading"]
 
+statement = """
+import json
+import sentry_sdk.integrations as si
+
+integrations = []
 if hasattr(si, '_AUTO_ENABLING_INTEGRATIONS'):
+    # _AUTO_ENABLING_INTEGRATIONS is a list of strings with default enabled integrations 
+    # https://github.com/getsentry/sentry-python/blob/c6b6f2086b58ffc674df5c25a600b8a615079fb5/sentry_sdk/integrations/__init__.py#L54-L66
+
     def make_integration_name(integration_name: str):
         return integration_name.rsplit(".", maxsplit=1)[0]
 
-    hiddenimports.extend(map(make_integration_name, si._AUTO_ENABLING_INTEGRATIONS))
+    print(json.dumps(integrations.extend(map(make_integration_name, si._AUTO_ENABLING_INTEGRATIONS))))
+"""
+
+hiddenimports.extend(json.loads(exec_statement(statement)))
+
