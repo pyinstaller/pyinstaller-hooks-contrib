c6ee15796c0215abc171c3239c14812d945fae47
Paul Müller <paulmueller@users.noreply.github.com>
2021-02-24 11:01:56 +0100

    narrowed down hook to essential files

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pyqtgraph.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pyqtgraph.py
@@ -10,7 +10,26 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
-# Data files
-datas = collect_data_files("pyqtgraph", include_py_files=True)
+# include all .ui and image files
+datas = collect_data_files("pyqtgraph",
+                           includes=["**/*.ui", "**/*.png", "**/*.svg"])
+
+# pyqtgraph uses Qt-version-specific templates for the UI elements.
+# There are templates for different versions of PySide and PyQt, e.g.
+#
+# - pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt5
+# - pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt6
+# - pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyside2
+# - pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyside6
+# - pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt5
+# - pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt6
+# - pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyside2
+# - pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyside6
+#
+# To be future-proof, we include all of them via a filter in
+# collect-submodules.
+# Tested with pyqtgraph master branch (commit c1900aa).
+hiddenimports = collect_submodules(
+    "pyqtgraph", filter=lambda name: "Template" in name)
