efcd17637d11fc0cde31f6656430a6fa7d1c3e2f
John McGuigan <jrm5100@psu.edu>
2021-03-15 16:53:40 -0400

    Simplify the hook

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-statsmodels.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-statsmodels.py
@@ -10,20 +10,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-hiddenimports=[
-'statsmodels.tsa.statespace._kalman_initialization',
-'statsmodels.tsa.statespace._kalman_filter',
-'statsmodels.tsa.statespace._kalman_smoother',
-'statsmodels.tsa.statespace._representation',
-'statsmodels.tsa.statespace._simulation_smoother',
-'statsmodels.tsa.statespace._statespace',
-'statsmodels.tsa.statespace._tools',
-'statsmodels.tsa.statespace._filters._conventional',
-'statsmodels.tsa.statespace._filters._inversions',
-'statsmodels.tsa.statespace._filters._univariate',
-'statsmodels.tsa.statespace._filters._univariate_diffuse',
-'statsmodels.tsa.statespace._smoothers._alternative',
-'statsmodels.tsa.statespace._smoothers._classical',
-'statsmodels.tsa.statespace._smoothers._conventional',
-'statsmodels.tsa.statespace._smoothers._univariate',
-'statsmodels.tsa.statespace._smoothers._univariate_diffuse']
+from PyInstaller.utils.hooks import collect_submodules
+
+hiddenimports = collect_submodules('statsmodels.tsa.statespace')
