da80aa695e6ae6bbbec96b2091384915b503bf2c
Hilali4886 <85049955+Hilali4886@users.noreply.github.com>
2021-05-31 07:34:44 +0300

    Update hook-azurerm.py
    delete space

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-azurerm.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-azurerm.py
@@ -9,11 +9,11 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
-
 # Azurerm is a lite api to microsoft azure.
 # Azurerm is using pkg_resources internally which is not supported by py-installer.
 # This hook will collect the module metadata.
 # Tested with Azurerm 0.10.0
+
 from PyInstaller.utils.hooks import copy_metadata
 
 datas = copy_metadata("azurerm")
