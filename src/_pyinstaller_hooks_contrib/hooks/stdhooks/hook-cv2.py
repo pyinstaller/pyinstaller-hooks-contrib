62630811fba308b59ab3355f6319aaddd65167db
eric15342335 <70310617+eric15342335@users.noreply.github.com>
2021-08-19 19:04:37 +0800

    add cv2 config file

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-cv2.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-cv2.py
@@ -16,13 +16,18 @@ import os
 from PyInstaller.utils.hooks import collect_dynamic_libs
 from PyInstaller import compat
 
-hiddenimports = ['numpy'] 
+hiddenimports = ['numpy']
+
+datas = []
+configs = os.path.join(exec_statement('import cv2; print(cv2.__path__[0])'), 'confi*.py')
+for g in glob.glob(configs):
+    datas.append((g, '.'))
 
 # On Windows, make sure that opencv_videoio_ffmpeg*.dll is bundled
 binaries = []
 if compat.is_win:
     # If conda is active, look for the DLL in its library path
-    if compat.is_conda:
+    if compat.is_pure_conda:
         libdir = os.path.join(compat.base_prefix, 'Library', 'bin')
         pattern = os.path.join(libdir, 'opencv_videoio_ffmpeg*.dll')
         for f in glob.glob(pattern):
