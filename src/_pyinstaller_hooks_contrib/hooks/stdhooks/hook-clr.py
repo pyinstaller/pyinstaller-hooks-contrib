bd5c09713dbd71df6c5bc9be327c5bc5bfbe67c1
Melvin Wang <t-melvinwang@microsoft.com>
2021-08-24 11:57:01 -0400

    address CR comments

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-clr.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-clr.py
@@ -19,28 +19,33 @@ but the latter isn't found by PyInstaller.
 
 import ctypes.util
 from PyInstaller.compat import is_win, getsitepackages
-from PyInstaller.utils.hooks import is_module_satisfies
+from PyInstaller.utils.hooks import is_module_satisfies, logger
 from pathlib import Path
+
+try:
+    from importlib.metadata import files
+except ImportError:
+    from import_lib.metadata import files
 
 # pythonnet is available for all platforms using .NET and Mono,
 # but tested only on Windows using .NET.
 
 if is_win:
-    pyruntime = 'Python.Runtime'
-    library = ctypes.util.find_library(pyruntime)
     datas = []
-    if library:
-        datas = [(library, '.')]
-    else:
-        # find Python.Runtime.dll in pip-installed pythonnet package
-        if is_module_satisfies('pythonnet < 3.0.0'):
-            pyruntime_path_prefix = ''
-        else:
-            pyruntime_path_prefix = 'pythonnet/runtime/'
 
-        for sitepack in [Path(s).resolve() for s in getsitepackages()]:
-            library = sitepack / f'{pyruntime_path_prefix}{pyruntime}.dll'
-            if library.exists():
-                datas = [(library, f'{pyruntime_path_prefix if not "" else "."}')]
-        if not datas:
-            raise Exception(pyruntime + ' not found')
+    filepaths =  [f for f in files('pythonnet') if 'Python.Runtime.dll' in str(f)]
+    if len(filepaths) == 1:
+        pyruntime_path = filepaths[0]
+        datas = [(pyruntime_path.locate(), pyruntime_path.parent.as_posix())]
+    elif len(filepaths) > 1:
+        logger.warning('More than one Python.Runtime.dll found in site packages! Cannot resolve.')
+
+    if len(datas) == 0:
+        # Fallback to legacy way of finding Python.Runtime dependency
+        library = ctypes.util.find_library('Python.Runtime')
+        if library:
+            datas = [(library, '.')]
+            logger.warning('Legacy method of finding Python.Runtime.dll was used!')
+
+    if not datas:
+        raise Exception('Python.Runtime.dll not found')
