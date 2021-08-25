1eaead36960e4e4060a024d3740eb09273b5801f
Melvin Wang <t-melvinwang@microsoft.com>
2021-08-25 09:47:05 -0400

    run hook on all platforms

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-clr.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-clr.py
@@ -18,7 +18,6 @@ but the latter isn't found by PyInstaller.
 
 
 import ctypes.util
-from PyInstaller.compat import is_win
 from PyInstaller.log import logger
 
 try:
@@ -26,25 +25,21 @@ try:
 except ImportError:
     from importlib_metadata import files
 
-# pythonnet is available for all platforms using .NET and Mono,
-# but tested only on Windows using .NET.
+datas = []
 
-if is_win:
-    datas = []
+filepaths = [f for f in files('pythonnet') if 'Python.Runtime.dll' in str(f)]
+if len(filepaths) == 1:
+    pyruntime_path = filepaths[0]
+    datas = [(pyruntime_path.locate(), pyruntime_path.parent.as_posix())]
+elif len(filepaths) > 1:
+    logger.warning('More than one Python.Runtime.dll found in site packages! Cannot resolve.')
 
-    filepaths = [f for f in files('pythonnet') if 'Python.Runtime.dll' in str(f)]
-    if len(filepaths) == 1:
-        pyruntime_path = filepaths[0]
-        datas = [(pyruntime_path.locate(), pyruntime_path.parent.as_posix())]
-    elif len(filepaths) > 1:
-        logger.warning('More than one Python.Runtime.dll found in site packages! Cannot resolve.')
+if len(datas) == 0:
+    # Fallback to legacy way of finding Python.Runtime dependency
+    library = ctypes.util.find_library('Python.Runtime')
+    if library:
+        datas = [(library, '.')]
+        logger.warning('Legacy method of finding Python.Runtime.dll was used!')
 
-    if len(datas) == 0:
-        # Fallback to legacy way of finding Python.Runtime dependency
-        library = ctypes.util.find_library('Python.Runtime')
-        if library:
-            datas = [(library, '.')]
-            logger.warning('Legacy method of finding Python.Runtime.dll was used!')
-
-    if not datas:
-        raise Exception('Python.Runtime.dll not found')
+if not datas:
+    raise Exception('Python.Runtime.dll not found')
