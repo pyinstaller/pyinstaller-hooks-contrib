63eb7bf8a215afd1764ba10e3ae29ee054cb7f32
bwoodsend <bwoodsend@gmail.com>
2021-11-18 09:26:46 +0000

    CI/CD: Add towncrier news fragment validation workflow.
    This is mostly copied from PyInstaller's repo.

---
+++ scripts/verify-news-fragments.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2021, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+"""
+Verify that new news entries have valid filenames. Usage:
+
+.. code-block:: bash
+
+    python scripts/verify-news-fragments.py
+
+"""
+
+import re
+import sys
+from pathlib import Path
+
+CHANGELOG_GUIDE = \
+    "https://github.com/pyinstaller/pyinstaller-hooks-contrib/tree/master/news#readme"
+
+CHANGE_TYPES = {
+    'new', 'update', 'process', 'tests'
+}
+
+NEWS_PATTERN = re.compile(r"(\d+)\.(\w+)\.(?:(\d+)\.)?rst")
+
+NEWS_DIR = Path(__file__).absolute().parent.parent / "news"
+
+
+def validate_name(name):
+    """
+    Check a filename/filepath matches the required format.
+
+    :param name: Name of news fragment file.
+    :type: str, os.Pathlike
+
+    :raises: ``SystemExit`` if above checks don't pass.
+    """
+    match = NEWS_PATTERN.fullmatch(Path(name).name)
+    if match is None:
+        raise SystemExit(
+            f"'{name}' does not match the '(pr-number).(type).rst' or '(pr-number).(type).(enumeration).rst' changelog "
+            f"entries formats. See:\n{CHANGELOG_GUIDE}"
+        )
+
+    if match.group(2) not in CHANGE_TYPES:
+        sys.exit("'{}' is of invalid type '{}'. Valid types are: {}".format(name, match.group(2), CHANGE_TYPES))
+
+    print(name, "is ok")
+
+
+def main():
+
+    for file in NEWS_DIR.iterdir():
+        if file.name in ("README.txt",  "_template.rst"):
+            continue
+        validate_name(file.name)
+
+    # Ensure that news items aren't left in the repo's root.
+    for file in NEWS_DIR.parent.iterdir():
+        if NEWS_PATTERN.match(file.name):
+            raise SystemExit("{file} is in the wrong directory. It should be moved to the 'news' directory.")
+
+
+if __name__ == "__main__":
+    main()
