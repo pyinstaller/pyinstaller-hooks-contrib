58d14282cef8413715da24a2c1d225e64d5d79e9
Tom Prince <tom.prince@hocat.ca>
2021-11-28 00:48:03 -0700

    Remove `twisted.internet.reactor` hook. (#356)
    * Add some tests for `twisted.internet.reactor`.

--- src/_pyinstaller_hooks_contrib/tests/test_libraries.py
+++ src/_pyinstaller_hooks_contrib/tests/test_libraries.py
@@ -995,3 +995,25 @@ def test_ffpyplayer(pyi_builder):
     pyi_builder.test_source("""
         import ffpyplayer.player
         """)
+
+
+@importorskip("twisted")
+def test_twisted_default_reactor(pyi_builder):
+    pyi_builder.test_source("""
+        from twisted.internet import reactor
+        assert callable(reactor.listenTCP)
+        """)
+
+
+@importorskip("twisted")
+def test_twisted_custom_reactor(pyi_builder):
+    pyi_builder.test_source("""
+        import sys
+        if sys.platform.startswith("win") and sys.version_info >= (3,7):
+            import asyncio
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        from twisted.internet import asyncioreactor
+        asyncioreactor.install()
+        from twisted.internet import reactor
+        assert callable(reactor.listenTCP)
+        """)
