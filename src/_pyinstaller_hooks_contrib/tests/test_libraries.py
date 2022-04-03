82830c30ca9fa1926555d290d90f5f79f72988a6
SMesser <SMesser@users.noreply.github.com>
2022-04-02 22:55:41 -0400

    incorporate minor suggestions for fiona-support pull request

--- src/_pyinstaller_hooks_contrib/tests/test_libraries.py
+++ src/_pyinstaller_hooks_contrib/tests/test_libraries.py
@@ -16,8 +16,8 @@ from PyInstaller.utils.hooks import is_module_satisfies, can_import_module
 from PyInstaller.utils.tests import importorskip, requires, xfail
 
 
-def test_fiona(test):
-    test.test_source(
+def test_fiona(pyi_builder):
+    pyi_builder.test_source(
         '''
         import fiona
         '''
