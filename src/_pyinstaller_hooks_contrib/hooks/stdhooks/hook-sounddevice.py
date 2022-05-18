095d3c34837cb944c12e571ff743eca535aaaa1c
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-05-18 19:20:55 +0200

    hooks: update sounddevice and soundfile hooks for PyInstaller 5.1
    Update hooks for PyInstaller 5.1 compatibility. As sounddevice and
    soundfile are not packages but top-level modules, the hookutils
    get_package_paths() now fails. Both hooks should have been using
    get_module_file_attribute() instead.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-sounddevice.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-sounddevice.py
@@ -19,18 +19,15 @@ https://github.com/spatialaudio/python-sounddevice/
 import os
 
 from PyInstaller.compat import is_darwin, is_win
-from PyInstaller.utils.hooks import get_package_paths
+from PyInstaller.utils.hooks import get_module_file_attribute
 
-sfp = get_package_paths("sounddevice")
+module_dir = os.path.dirname(get_module_file_attribute("sounddevice"))
 
 path = None
 if is_win:
-    path = os.path.join(sfp[0], "_sounddevice_data", "portaudio-binaries")
+    path = os.path.join(module_dir, "_sounddevice_data", "portaudio-binaries")
 elif is_darwin:
-    path = os.path.join(
-        sfp[0], "_sounddevice_data", "portaudio-binaries", "libportaudio.dylib"
-    )
+    path = os.path.join(module_dir, "_sounddevice_data", "portaudio-binaries", "libportaudio.dylib")
 
 if path is not None and os.path.exists(path):
-    binaries = [(path,
-                 os.path.join("_sounddevice_data", "portaudio-binaries"))]
+    binaries = [(path, os.path.join("_sounddevice_data", "portaudio-binaries"))]
