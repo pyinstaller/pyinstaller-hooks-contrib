991a0a22e51486feae7fd3c05c43236be0df2f42
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-09-20 14:35:36 +0200

    hooks: sounddevice and soundfile: collect system-installed shared lib
    Extend the sounddevice and soundfile hooks to collect system-installed
    sndfile and portaudio shared libraries in cases when the libraries
    are not bundled with the package (i.e., linux PyPI wheels and
    Anaconda-provided packages on all OSes).

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-soundfile.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-soundfile.py
@@ -15,21 +15,49 @@ pysoundfile:
 https://github.com/bastibe/SoundFile
 """
 
-import os
+import pathlib
 
-from PyInstaller.compat import is_win, is_darwin
-from PyInstaller.utils.hooks import get_module_file_attribute
+from PyInstaller.utils.hooks import get_module_file_attribute, logger
 
-# get path of soundfile
-module_dir = os.path.dirname(get_module_file_attribute('soundfile'))
+binaries = []
+datas = []
 
-# add binaries packaged by soundfile on OSX and Windows
-# an external dependency (libsndfile) is used on GNU/Linux
-path = None
-if is_win:
-    path = os.path.join(module_dir, '_soundfile_data')
-elif is_darwin:
-    path = os.path.join(module_dir, '_soundfile_data', 'libsndfile.dylib')
+# PyPI wheels for Windows and macOS ship the sndfile shared library in _soundfile_data directory,
+# located next to the soundfile.py module file (i.e., in the site-packages directory).
+module_dir = pathlib.Path(get_module_file_attribute('soundfile')).parent
+data_dir = module_dir / '_soundfile_data'
+if data_dir.is_dir():
+    destdir = str(data_dir.relative_to(module_dir))
 
-if path is not None and os.path.exists(path):
-    binaries = [(path, "_soundfile_data")]
+    # Collect the shared library (known variants: libsndfile64bit.dll, libsndfile32bit.dll, libsndfile.dylib)
+    for lib_file in data_dir.glob("libsndfile*.*"):
+        binaries += [(str(lib_file), destdir)]
+
+    # Collect the COPYING file
+    copying_file = data_dir / "COPYING"
+    if copying_file.is_file():
+        datas += [(str(copying_file), destdir)]
+else:
+    # On linux and in Anaconda in all OSes, the system-installed sndfile library needs to be collected.
+    def _find_system_sndfile_library():
+        import os
+        import ctypes.util
+        from PyInstaller.depend.utils import _resolveCtypesImports
+
+        libname = ctypes.util.find_library("sndfile")
+        if libname is not None:
+            resolved_binary = _resolveCtypesImports([os.path.basename(libname)])
+            if resolved_binary:
+                return resolved_binary[0][1]
+
+    try:
+        lib_file = _find_system_sndfile_library()
+    except Exception as e:
+        logger.warning("Error while trying to find system-installed sndfile library: %s", e)
+        lib_file = None
+
+    if lib_file:
+        binaries += [(lib_file, '.')]
+
+if not binaries:
+    logger.warning("sndfile shared library not found - soundfile will likely fail to work!")
