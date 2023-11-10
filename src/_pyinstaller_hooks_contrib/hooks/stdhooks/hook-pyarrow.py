866cdd471c5bb4b0f7c6577f173c2b4090304607
Nicolas Elie <40382614+n-elie@users.noreply.github.com>
2023-11-10 10:52:42 +0100

    Update hook-pyarrow.py

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pyarrow.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pyarrow.py
@@ -14,6 +14,6 @@
 
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files, collect_dynamic_libs
 
-hiddenimports = collect_submodules('pyarrow')
+hiddenimports = collect_submodules('pyarrow', filter=lambda x: "tests" not in x.split("."))
 datas = collect_data_files('pyarrow')
 binaries = collect_dynamic_libs('pyarrow')
