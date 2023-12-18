6cb34e6a78b8a1368297cbef80bf96605cafe15b
Rok Mandeljc <rok.mandeljc@gmail.com>
2023-12-18 22:54:45 +0100

    hooks: sudachipy: add hiddenimports for v0.6.8
    `sudachipy˙ 0.6.8 added `sudachipy.config` and `sudachipy.errors`
    modules, which are referenced from the binary extension.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-sudachipy.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-sudachipy.py
@@ -10,10 +10,17 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import can_import_module, collect_data_files
+from PyInstaller.utils.hooks import can_import_module, collect_data_files, is_module_satisfies
 
 datas = collect_data_files('sudachipy')
 hiddenimports = []
+
+# In v0.6.8, `sudachipy.config` and `sudachipy.errors` modules were added, and are referenced from binary extension.
+if is_module_satisfies('sudachipy >= 0.6.8'):
+    hiddenimports += [
+        'sudachipy.config',
+        'sudachipy.errors',
+    ]
 
 # Check which types of dictionary are installed
 for sudachi_dict in ['sudachidict_small', 'sudachidict_core', 'sudachidict_full']:
