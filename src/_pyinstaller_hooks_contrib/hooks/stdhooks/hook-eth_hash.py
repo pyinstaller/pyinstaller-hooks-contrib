1b5deb045af5de6cf3051b3a311c3f09283dc5e2
Brénainn Woodsend <bwoodsend@gmail.com>
2024-01-15 19:40:53 +0000

    Collect metadata for eth-hash>=0.6

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-eth_hash.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-eth_hash.py
@@ -10,7 +10,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_submodules
+from PyInstaller.utils.hooks import collect_submodules, copy_metadata, is_module_satisfies
 
 # The ``eth_hash.utils.load_backend`` function does a dynamic import.
 hiddenimports = collect_submodules('eth_hash.backends')
+
+# As of eth-hash 0.6.0, it uses importlib.metadata.version() set its __version__ attribute
+if is_module_satisfies("eth-hash >= 0.6.0"):
+    datas = copy_metadata("eth-hash")
