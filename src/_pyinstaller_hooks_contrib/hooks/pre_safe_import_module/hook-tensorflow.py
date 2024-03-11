1ead9a7c4c5621284d28ccb157655b11c45b1fbc
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-03-11 22:47:42 +0100

    hooks: update tensorflow hook for compatibility with tensorflow 2.16.0
    Starting with `tensorflow` 2.16.0, the `tensorflow.keras` alias
    points to `keras._tf_keras.keras`; so update the pre-safe-import
    hook accordingly.
    
    Failing to do so prevents a program that imports only from
    `tensorflow.keras` from collecting `tensorflow`.

--- src/_pyinstaller_hooks_contrib/hooks/pre_safe_import_module/hook-tensorflow.py
+++ src/_pyinstaller_hooks_contrib/hooks/pre_safe_import_module/hook-tensorflow.py
@@ -19,5 +19,9 @@ def pre_safe_import_module(api):
     # See: https://github.com/pyinstaller/pyinstaller/discussions/6890
     # The alias was already present in earlier releases, but it does not seem to be causing problems there,
     # so keep this specific to tensorflow >= 2.8.0 to avoid accidentally breaking something else.
-    if is_module_satisfies("tensorflow >= 2.8.0"):
+    #
+    # Starting with tensorflow 2.16.0, the alias points to `keras._tf_keras.keras`.
+    if is_module_satisfies("tensorflow >= 2.16.0"):
+        api.add_alias_module('keras._tf_keras.keras', 'tensorflow.keras')
+    elif is_module_satisfies("tensorflow >= 2.8.0"):
         api.add_alias_module('keras.api._v2.keras', 'tensorflow.keras')
