5a7c558df7367b4789d6e8cc78d928074c1f6bc4
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-03-20 11:46:26 +0100

    hooks: numba: ensure numba.cloudpickle.cloudpickle_fast is collected
    `numba` 0.59.0 updated its vendored version of `cloudpickle` to 3.0.0;
    this version keeps `cloudpickle_fast` module around for backward
    compatibility with existing pickled data, but does not import it
    directly anymore.
    
    Therefore, we need to add it to hidden imports to ensure that it is
    collected and available when unpickling data that was pickled with
    older version of `numba`.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-numba.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-numba.py
@@ -16,5 +16,12 @@
 # Tested with:
 # numba 0.26 (Anaconda 4.1.1, Windows), numba 0.28 (Linux)
 
+from PyInstaller.utils.hooks import is_module_satisfies
+
 excludedimports = ["IPython", "scipy"]
 hiddenimports = ["llvmlite"]
+
+# numba 0.59.0 updated its vendored version of cloudpickle to 3.0.0; this version keeps `cloudpickle_fast` module
+# around for backward compatibility with existing pickled data, but does not import it directly anymore.
+if is_module_satisfies("numba >= 0.59.0"):
+    hiddenimports += ["numba.cloudpickle.cloudpickle_fast"]
