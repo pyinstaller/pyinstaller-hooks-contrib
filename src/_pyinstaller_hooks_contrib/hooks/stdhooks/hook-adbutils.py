5f5e2bcaaed1469d2f1a55df224d12ebdab3d2f9
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-03-25 21:38:34 +0100

    hooks: update adbutils hook for compatibility with adbutils >= 2.2.2
    With `adbutils` >= 2.2.2, we need a hidden import for
    `adbutils.binaries`, so that `importlib.resources` can determine
    the package's path.

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-adbutils.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-adbutils.py
@@ -10,7 +10,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, is_module_satisfies
 
 # adb.exe is not automatically collected by collect_dynamic_libs()
 datas = collect_data_files("adbutils", subdir="binaries", includes=["adb*"])
+
+# adbutils v2.2.2 replaced `pkg_resources` with `importlib.resources`, and now uses the following code to determine the
+# path to the `adbutils.binaries` sub-package directory:
+# https://github.com/openatx/adbutils/blob/2.2.2/adbutils/_utils.py#L78-L87
+# As `adbutils.binaries` is not directly imported anywhere, we need a hidden import.
+if is_module_satisfies('adbutils >= 2.2.2'):
+    hiddenimports = ['adbutils.binaries']
