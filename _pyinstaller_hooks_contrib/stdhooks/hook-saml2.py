2629e1c694c85ab029ce2fe21754ffb5963d76e2
Kamil Frydel <k.frydel@gmail.com>
2024-09-12 13:26:14 +0200

    Review fix - GH links in single lines

--- _pyinstaller_hooks_contrib/stdhooks/hook-saml2.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-saml2.py
@@ -16,12 +16,10 @@ from PyInstaller.utils.hooks import collect_data_files, copy_metadata, collect_s
 datas = copy_metadata("pysaml2")
 
 # The library contains a bunch of XSD schemas that are loaded by the code:
-# https://github.com/IdentityPython/pysaml2/blob/7cb4f09dce87a7e8098b9c7552ebab8bc77bc896
-# /src/saml2/xml/schema/__init__.py#L23
+# https://github.com/IdentityPython/pysaml2/blob/7cb4f09dce87a7e8098b9c7552ebab8bc77bc896/src/saml2/xml/schema/__init__.py#L23
 # On the other hand, runtime tools are not needed.
 datas += collect_data_files("saml2", excludes=["**/tools"])
 
 # Submodules are loaded dynamically by:
-# https://github.com/IdentityPython/pysaml2/blob/7cb4f09dce87a7e8098b9c7552ebab8bc77bc896
-# /src/saml2/attribute_converter.py#L52
+# https://github.com/IdentityPython/pysaml2/blob/7cb4f09dce87a7e8098b9c7552ebab8bc77bc896/src/saml2/attribute_converter.py#L52
 hiddenimports = collect_submodules("saml2.attributemaps")
