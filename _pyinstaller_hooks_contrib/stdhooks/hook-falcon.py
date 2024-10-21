d92bb804102545ecbca634abb45c13435db85fce
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-10-21 22:16:34 +0200

    hooks: update falcon hook for compatibility with falcon v4.0.0
    `falcon` v4.0.0 added couple of more cythonized modules that depend
    on `dataclasses`, `json`, and `wsgiref.types` modules from stdlib;
    so we need to add those to `hiddenimports`.

--- _pyinstaller_hooks_contrib/stdhooks/hook-falcon.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-falcon.py
@@ -10,6 +10,9 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
+from PyInstaller.compat import is_py311
+from PyInstaller.utils.hooks import is_module_satisfies
+
 hiddenimports = [
     'cgi',
     'falcon.app_helpers',
@@ -25,3 +28,14 @@ hiddenimports = [
     'xml.etree.ElementTree',
     'xml.etree'
 ]
+
+# falcon v4.0.0 added couple of more cythonized modules that depend on the following stdlib modules.
+if is_module_satisfies('falcon >= 4.0.0'):
+    hiddenimports += [
+        'dataclasses',
+        'json',
+    ]
+
+    # `wsgiref.types` is available (and thus referenced) only under python >= 3.11.
+    if is_py311:
+        hiddenimports += ['wsgiref.types']
