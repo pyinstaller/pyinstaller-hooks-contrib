7bff95b42fbde8cb90deb9cb0f7b8577eb5ebefc
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-01-17 13:04:15 +0100

    hooks: update `numba` hook for compatibility with numba v0.61
    Add hidden imports for dynamically redirected and loaded modules
    that were introduced as part of the new type system in `numba`
    v0.61.0.
    
    Extend `numba` tests with a basic test that uses `numba.jit`,
    and a basic import test with `NUMBA_USE_LEGACY_TYPE_SYSTEM` set
    to 0.

--- _pyinstaller_hooks_contrib/stdhooks/hook-numba.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-numba.py
@@ -25,3 +25,30 @@ hiddenimports = ["llvmlite"]
 # around for backward compatibility with existing pickled data, but does not import it directly anymore.
 if is_module_satisfies("numba >= 0.59.0"):
     hiddenimports += ["numba.cloudpickle.cloudpickle_fast"]
+
+# numba 0.61 introduced new type system with several dynamic redirects using `numba.core.utils._RedirectSubpackage`;
+# depending on the run-time value of `numba.config.USE_LEGACY_TYPE_SYSTEM`, either "old" or "new" module variant is
+# loaded. All of these seem to be loaded when `numba` is imported, so there is no need for finer granularity. Also,
+# as the config value might be manipulated at run-time (e.g., via environment variable), we need to collect both old
+# and new module variants.
+if is_module_satisfies("numba >= 0.61.0rc1"):
+    # NOTE: `numba.core.typing` is also referenced indirectly via `_RedirectSubpackage`, but we do not need a
+    # hidden import entry for it, because we have entries for its submodules.
+    modules_old = [
+        'numba.core.datamodel.old_models',
+        'numba.core.old_boxing',
+        'numba.core.types.old_scalars',
+        'numba.core.typing.old_builtins',
+        'numba.core.typing.old_cmathdecl',
+        'numba.core.typing.old_mathdecl',
+        'numba.cpython.old_builtins',
+        'numba.cpython.old_hashing',
+        'numba.cpython.old_mathimpl',
+        'numba.cpython.old_numbers',
+        'numba.cpython.old_tupleobj',
+        'numba.np.old_arraymath',
+        'numba.np.random.old_distributions',
+        'numba.np.random.old_random_methods',
+    ]
+    modules_new = [name.replace('.old_', '.new_') for name in modules_old]
+    hiddenimports += modules_old + modules_new
