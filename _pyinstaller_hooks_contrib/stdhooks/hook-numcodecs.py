8656e8f0e6489a887cf07c9e0b852063feb6ee9f
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-01-20 19:34:09 +0100

    hooks: update numcodecs hook for compatibility with numcodecs v0.15.0
    Add a hidden import for `deprecated` module, which was added to
    one of cythonized extension in `numcodecs` v0.15.0.

--- _pyinstaller_hooks_contrib/stdhooks/hook-numcodecs.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-numcodecs.py
@@ -10,5 +10,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
+from PyInstaller.utils.hooks import is_module_satisfies
+
 # compat_ext is only imported from pyx files, so it is missed
 hiddenimports = ['numcodecs.compat_ext']
+
+# numcodecs v0.15.0 added an import of `deprecated` (from `Deprecated` dist) in one of its cythonized extension.
+if is_module_satisfies('numcodecs >= 0.15.0'):
+    hiddenimports += ['deprecated']
