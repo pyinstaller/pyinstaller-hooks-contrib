30c1dfbb6b986cf161e6a5cf5a5960626bb01b81
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-03-17 23:15:47 +0100

    hooks: update toga hook for compatibility with toga v0.5.0
    The main module of `toga` (the `__init__.py`) has been refactored
    in v0.5.0, and direct imports of packages (sub)modules have been
    replaced with lazy loading/importing.
    
    Therefore, we now need to explicitly collect the package's submodules
    using the `collect_submodules` helper.

--- _pyinstaller_hooks_contrib/stdhooks/hook-toga.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-toga.py
@@ -11,7 +11,9 @@
 # ------------------------------------------------------------------
 
 from PyInstaller import compat
-from PyInstaller.utils.hooks import copy_metadata
+from PyInstaller.utils.hooks import collect_submodules, copy_metadata, is_module_satisfies
+
+hiddenimports = []
 
 # Select the platform-specific backend.
 if compat.is_darwin:
@@ -24,10 +26,15 @@ else:
     backend = None
 
 if backend is not None:
-    hiddenimports = [f'toga_{backend}', f'toga_{backend}.factory']
+    hiddenimports += [f'toga_{backend}', f'toga_{backend}.factory']
 
 # Collect metadata for toga-core dist, which is used by toga module to determine its version.
 datas = copy_metadata("toga-core")
 
 # Prevent `toga` from pulling `setuptools_scm` into frozen application, as it makes no sense in that context.
 excludedimports = ["setuptools_scm"]
+
+# `toga` 0.5.0 refactored its `__init__.py` to lazy-load its core modules. Therefore, we now need to collect
+# submodules via `collect_submodules`...
+if is_module_satisfies("toga >= 0.5.0"):
+    hiddenimports += collect_submodules("toga")
