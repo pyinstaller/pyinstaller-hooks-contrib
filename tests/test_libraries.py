384b16d57b6da1bf641e786c3121860667f5f854
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-03-30 22:17:13 +0200

    tests: add a test for pandera
    Add test for `pandera`, based on the Quick Start example from its
    documentation. At the moment, it does not need its own hook, but
    it does require the ones from the previous commits.

--- tests/test_libraries.py
+++ tests/test_libraries.py
@@ -2792,3 +2792,35 @@ def test_frictionless(pyi_builder, tmp_path):
         output = frictionless.validate(filename)
         pprint.pprint(output)
     """, app_args=[str(csv_file)])
+
+
+@importorskip('pandas')
+@importorskip('pandera')
+def test_pandera(pyi_builder):
+    # Example from pandera's Quick Start
+    pyi_builder.test_source("""
+        import pandas as pd
+        import pandera as pa
+
+        # data to validate
+        df = pd.DataFrame({
+            "column1": [1, 4, 0, 10, 9],
+            "column2": [-1.3, -1.4, -2.9, -10.1, -20.4],
+            "column3": ["value_1", "value_2", "value_3", "value_2", "value_1"]
+        })
+
+        # define schema
+        schema = pa.DataFrameSchema({
+            "column1": pa.Column(int, checks=pa.Check.le(10)),
+            "column2": pa.Column(float, checks=pa.Check.lt(-1.2)),
+            "column3": pa.Column(str, checks=[
+                pa.Check.str_startswith("value_"),
+                # define custom checks as functions that take a series as input and
+                # outputs a boolean or boolean Series
+                pa.Check(lambda s: s.str.split("_", expand=True).shape[1] == 2)
+            ]),
+        })
+
+        validated_df = schema(df)
+        print(validated_df)
+    """)
