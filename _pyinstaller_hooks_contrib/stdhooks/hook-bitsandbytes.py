1065009c05a627729ae6d2c2fd0ecfed30bffe1f
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-04-03 22:44:14 +0200

    cleanup: replace occurrences of U+02D9 with backtick character
    Replace occurrences of the "dot above" (U+02D9) character with
    the intended backtick character.

--- _pyinstaller_hooks_contrib/stdhooks/hook-bitsandbytes.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-bitsandbytes.py
@@ -14,7 +14,7 @@ from PyInstaller.utils.hooks import collect_dynamic_libs
 
 # bitsandbytes contains several extensions for CPU and different CUDA versions: libbitsandbytes_cpu.so,
 # libbitsandbytes_cuda110_nocublaslt.so, libbitsandbytes_cuda110.so, etc. At build-time, we could query the
-# `bitsandbytes.cextension.setup` and its `binary_name˙ attribute for the extension that is in use. However, if the
+# `bitsandbytes.cextension.setup` and its `binary_name` attribute for the extension that is in use. However, if the
 # build system does not have CUDA available, this would automatically mean that we will not collect any of the CUDA
 # libs. So for now, we collect them all.
 binaries = collect_dynamic_libs("bitsandbytes")
