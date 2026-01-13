32f61aa8b891a3713ff1d04b386bef97f13c50da
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-01-13 21:20:48 +0100

    hooks: update langchain hook for compatibility with langchain >= 1.2.1
    `langchain` 1.2.1 refactored import logic for optional add-on
    packages that provide individual backeds; the direct imports made
    under conditional blocks were replaced with dictionary-based lookup
    and programmatic import, so our import analysis cannot pick them
    up anymore.
    
    Update the hook to read these same `_SUPPORTED_PROVIDERS` dictionaries
    (found in `langchain.chat_models.base` and `langchain.embeddings.base`)
    and construct the list of hiddenimports, in order to ensure that all
    installed add-on `langchain_` packages contine to be automatically
    collected.

--- _pyinstaller_hooks_contrib/stdhooks/hook-langchain.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-langchain.py
@@ -10,8 +10,36 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, is_module_satisfies
 
 # This was required with langchain < 1.0.0; in contemporary versions, the package does not contain any data files,
 # so this should be effectively a no-op.
 datas = collect_data_files('langchain')
+
+# In langchain 1.2.1 the import logic for optional add-on packages was refactored, and direct imports in conditional
+# blocks were replaced with dictionary-based look-up; see https://github.com/langchain-ai/langchain/pull/32813
+# So now we need to use that same dictionary to explicitly collect the optional packages that happen to be installed
+# in the build environment.
+if is_module_satisfies('langchain >= 1.2.1'):
+    from PyInstaller import isolated
+
+    @isolated.decorate
+    def get_optional_packages():
+        packages = set()
+
+        try:
+            from langchain.chat_models.base import _SUPPORTED_PROVIDERS
+            packages.update(package_name for package_name, *_ in _SUPPORTED_PROVIDERS.values())
+        except Exception:
+            pass
+
+        try:
+            from langchain.embeddings.base import _SUPPORTED_PROVIDERS
+            packages.update(package_name for package_name, *_ in _SUPPORTED_PROVIDERS.values())
+        except Exception:
+            pass
+
+        return sorted(packages)
+
+    hiddenimports = get_optional_packages()
+    warn_on_missing_hiddenimports = False
