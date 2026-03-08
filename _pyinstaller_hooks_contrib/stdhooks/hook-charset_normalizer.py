e327d75e53d0d6466e282b5aaf2eae93417894b2
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-03-08 12:58:33 +0100

    hooks: update charset_normalizer hook
    Update `charset-normalizer` hook for compatibility with
    `charset-normalizer` v3.4.5; the `mypy` is now dynamically-named,
    so we need to use the `find_mypyc_module_for_dist()` helper.

--- _pyinstaller_hooks_contrib/stdhooks/hook-charset_normalizer.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-charset_normalizer.py
@@ -12,5 +12,8 @@
 
 from PyInstaller.utils.hooks import is_module_satisfies
 
-if is_module_satisfies("charset_normalizer >= 3.0.1"):
+if is_module_satisfies("charset_normalizer >= 3.4.5"):
+    from _pyinstaller_hooks_contrib.utils.mypy import find_mypyc_module_for_dist
+    hiddenimports = find_mypyc_module_for_dist('charset_normalizer')
+elif is_module_satisfies("charset_normalizer >= 3.0.1"):
     hiddenimports = ["charset_normalizer.md__mypyc"]
