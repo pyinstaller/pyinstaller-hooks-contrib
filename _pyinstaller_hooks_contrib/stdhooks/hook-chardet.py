9065015d1bec46be74c249aea034cfd5cca9913c
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-03-30 22:28:50 +0200

    hooks: update chardet hook for compatibility with chardet v7.4.0
    The Windows PyPI wheel of `chardet` v7.4.0(post2) seems to have
    split extensions in `chardet.pipeline` into individual `__mypyc`
    extensions; for example, `chardet.pipeline.orchestrator` now
    references corresponding `chardet.pipeline.orchestrator__mypyc`.
    
    Add a `collect_submodules('chardet.pipeline')` to the hook to
    ensure these additional mypy extensions are always collected.

--- _pyinstaller_hooks_contrib/stdhooks/hook-chardet.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-chardet.py
@@ -12,10 +12,20 @@
 
 from PyInstaller.utils.hooks import is_module_satisfies
 
+hiddenimports = []
+datas = []
+
 # Starting with v7.0.0, we need to collect mypy extension, and the data files from chardet/models
 if is_module_satisfies("chardet >= 7.0.0"):
     from PyInstaller.utils.hooks import collect_data_files
     from _pyinstaller_hooks_contrib.utils.mypy import find_mypyc_module_for_dist
 
-    hiddenimports = find_mypyc_module_for_dist('chardet')
-    datas = collect_data_files('chardet')
+    hiddenimports += find_mypyc_module_for_dist('chardet')
+    datas += collect_data_files('chardet')
+
+# In the Windows PyPI wheel of chardet v7.4.0post2, the extensions in `chardet.pipeline` seem to come with individual
+# `__mypyc` counterparts (for example the `chardet.pipeline.orchestrator` extension references a corresponding
+# `chardet.pipeline.orchestrator__mypyc` extension). Ensure all these are collected by collecting submodules.
+if is_module_satisfies("chardet >= 7.4.0"):
+    from PyInstaller.utils.hooks import collect_submodules
+    hiddenimports += collect_submodules('chardet.pipeline')
