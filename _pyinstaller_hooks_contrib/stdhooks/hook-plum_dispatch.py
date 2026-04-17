42519234f31021c1cf1c86c04882c4fe7046c4dc
jessamynsmith <jessamyn.smith@gmail.com>
2026-04-17 09:48:17 -0400

    Replace collect_all with collect_submodules, and alter news accordingly for plum-dispatch

--- _pyinstaller_hooks_contrib/stdhooks/hook-plum_dispatch.py
+++ _pyinstaller_hooks_contrib/stdhooks/hook-plum_dispatch.py
@@ -10,9 +10,9 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 from _pyinstaller_hooks_contrib.utils.mypy import find_mypyc_module_for_dist
-from PyInstaller.utils.hooks import collect_all
+from PyInstaller.utils.hooks import collect_submodules
 
 # plum-dispatch uses `mypyc`, and includes a top-level module with
 # dynamically-generated name prefix; e.g., `ddc459050edb75a05942__mypyc`.
-datas, binaries, hiddenimports = collect_all('plum')
+hiddenimports = collect_submodules('plum')
 hiddenimports += find_mypyc_module_for_dist('plum-dispatch')
